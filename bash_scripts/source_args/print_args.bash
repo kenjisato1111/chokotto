3ef750edd97405e293bd538da2f28028987a192a
Kenji Sato <jskhoujou@gmail.com>
2018-12-19 18:49:54 +0900

    create source_args

---
+++ bash_scripts/source_args/print_args.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+first=${1:-faasuto}
+second=${2:-sekando}
+
+echo 'first :' $first ', second :' $second
---
+++ bash_scripts/source_args/source_print_args.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+source ./print_args.bash
+
+echo 'source first :' $1 ', source second :' $2
